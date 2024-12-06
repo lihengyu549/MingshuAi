fc0784285a6361413f6e5fd22daff474869d5a57
刘郑昊 <1242019907@qq.com>
2024-12-06 17:47:54 +0800

    1

--- src/views/system/dataSourceManagement/index.vue
+++ src/views/system/dataSourceManagement/index.vue
@@ -28,22 +28,6 @@
         <el-button type="primary" plain icon="el-icon-plus" size="mini" @click="handleAdd"
           v-hasPermi="['system:proxys:add']">新增</el-button>
       </el-col>
-      <!-- <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['system:proxys:edit']"
-        >修改</el-button>
-      </el-col> -->
-      <!-- <el-col :span="1.5">
-        <el-button type="danger" plain icon="el-icon-delete" size="mini" :disabled="multiple" @click="handleDelete"
-          v-hasPermi="['system:proxys:remove']">删除</el-button>
-      </el-col> -->
-
       <el-col :span="1.5">
         <el-button type="warning" plain icon="el-icon-download" size="mini" @click="deliveryStrategy"
           v-hasPermi="['system:proxys:export']">一键下发策略</el-button>
