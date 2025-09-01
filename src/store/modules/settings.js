450739441a37881310993b2210c13de96fbf8386
李恒宇 <hengy549@163.com>
2025-09-01 13:52:33 +0800

    修改背景颜色

--- src/store/modules/settings.js
+++ src/store/modules/settings.js
@@ -5,7 +5,7 @@ const { sideTheme, showSettings, topNav, tagsView, fixedHeader, sidebarLogo, dyn
 const storageSetting = JSON.parse(localStorage.getItem('layout-setting')) || ''
 const state = {
   title: '',
-  bgcColor: 'rgb(0, 84, 217)',
+  bgcColor: '#f9fafd',
   theme: storageSetting.theme || '#409EFF',
   sideTheme: storageSetting.sideTheme || sideTheme,
   showSettings: showSettings,
