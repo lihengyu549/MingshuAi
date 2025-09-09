026cce6ba7385da671a7ba4f230c798222f9b1d1
李恒宇 <hengy549@163.com>
2025-09-09 14:13:50 +0800

    账号只能给一个角色，一个角色是只读，一个角色是删除，这两个给一个账号会出现问题

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -282,7 +282,7 @@
           </el-col>
           <el-col :span="12">
             <el-form-item label="角色">
-              <el-select v-model="form.roleIds" multiple placeholder="请选择角色">
+              <el-select v-model="form.roleIds" placeholder="请选择角色">
                 <el-option
                   v-for="item in roleOptions"
                   :key="item.roleId"
