e8b98757d0696cf5212dbb1580eddb9fb1420b6f
李恒宇 <hengy549@163.com>
2025-12-19 11:35:46 +0800

    图片路径调整

--- src/App.vue
+++ src/App.vue
@@ -28,8 +28,7 @@ export default {
     getInterfaceDesignByIdFn(){
       getInterfaceDesignById().then(res => {
         let facvionEl = document.getElementById('facicon')
-        res.data.img = window.location.protocol + '//' + window.location.hostname + ':7101/' + res.data.img
-        // res.data.img = 'http://192.168.2.174:7101/' + res.data.img
+        res.data.img = window.location.origin + res.data.img
         facvionEl.href = res.data.img
         document.title = res.data.projectName
         this.$store.commit('SET_PROJECT', res.data);
